dde01f562721db244178bd241c0b59f3b63a2d11
zou_wy <zou_wy@centit.com>
2018-07-16 15:39:06 +0800

    url 不对应

--- msgpusher-client/src/main/java/com/centit/msgpusher/client/MsgPusherClientImpl.java
+++ msgpusher-client/src/main/java/com/centit/msgpusher/client/MsgPusherClientImpl.java
@@ -149,7 +149,7 @@ public class MsgPusherClientImpl implements MsgPusherClient {
     }
 
     public String userNotifySetting(CloseableHttpClient httpClient, UserNotifySetting notifySetting) throws Exception {
-        String jsonStr = HttpExecutor.jsonPost(httpClient, appSession.completeQueryUrl("/msgdlvry/userNotifySetting"), notifySetting);
+        String jsonStr = HttpExecutor.jsonPost(httpClient, appSession.completeQueryUrl("/notifysetting"), notifySetting);
         return jsonStr;
     }
 
